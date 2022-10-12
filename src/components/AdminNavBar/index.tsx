2fd6a32f2a30f82e3a12cf20f206da915c5a3666
Allef Douglas <allefwalker73@gmail.com>
2022-10-11 23:53:44 -0300

    feat: make logo clickable and redirect to admin home page

--- src/components/AdminNavBar/index.tsx
+++ src/components/AdminNavBar/index.tsx
@@ -1,10 +1,11 @@
+import { AdminRoutes } from '@/routes/admin-routes'
 import { Button } from '../Form/Button'
 import * as S from './styles'
 
 export const AdminNavBar = () => {
   return (
     <S.Container>
-      <h1>Logo</h1>
+      <S.Logo to={AdminRoutes.Admin_Home}>Logo</S.Logo>
       <Button variant="outlined">Sair</Button>
     </S.Container>
   )
--- src/components/AdminNavBar/styles.ts
+++ src/components/AdminNavBar/styles.ts
@@ -1,5 +1,8 @@
+import { Link } from 'react-router-dom'
 import tw, { styled } from 'twin.macro'
 
 export const Container = styled.div([
   tw`h-16 border-b px-8 flex items-center justify-between`,
 ])
+
+export const Logo = styled(Link)([tw``])
