9c79d09c71a556183b8fd31dedffeb0754c5ca60
Allef Douglas <allefwalker73@gmail.com>
2022-10-27 01:36:34 -0300

    feat: create AddressForm component

---
+++ src/components/AddressForm/styles.ts
@@ -0,0 +1,5 @@
+import tw, { styled } from 'twin.macro'
+
+export const Container = styled.div([tw`w-full flex flex-col gap-4`])
+
+export const Row = styled.div([tw`w-full flex items-center gap-4`])
