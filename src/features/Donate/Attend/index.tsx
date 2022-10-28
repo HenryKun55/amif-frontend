5597475e03e95ecde11fd828bf0c7d01edd7f720
Flávio Andrade <flavio.andrade@distrito.me>
2022-10-28 19:13:24 -0300

    feat: add animation when numbers are visible in AttendDonate

--- src/features/Donate/Attend/index.tsx
+++ src/features/Donate/Attend/index.tsx
@@ -1,6 +1,7 @@
 import CountUp from 'react-countup'
 import { FaChild } from 'react-icons/fa'
 import { GiHeavyArrow } from 'react-icons/gi'
+import VisibilitySensor from 'react-visibility-sensor'
 import { MdFamilyRestroom, MdVolunteerActivism } from 'react-icons/md'
 import * as S from './styles'
 
@@ -11,7 +12,13 @@ export const AttendDonate = () => {
         <S.Content>
           <FaChild size={40} />
           <S.Title>
-            <CountUp start={0} end={50} duration={2} prefix="+" />
+            <CountUp redraw end={50} duration={1.3} prefix="+">
+              {({ countUpRef, start }) => (
+                <VisibilitySensor onChange={start} delayedCall>
+                  <span ref={countUpRef} />
+                </VisibilitySensor>
+              )}
+            </CountUp>
             mil
           </S.Title>
           <S.Description>crianças</S.Description>
@@ -19,21 +26,39 @@ export const AttendDonate = () => {
         <S.Content>
           <MdFamilyRestroom size={40} />
           <S.Title>
-            <CountUp start={0} end={300} duration={2} prefix="+" />
+            <CountUp redraw end={300} duration={1.3} prefix="+">
+              {({ countUpRef, start }) => (
+                <VisibilitySensor onChange={start} delayedCall>
+                  <span ref={countUpRef} />
+                </VisibilitySensor>
+              )}
+            </CountUp>
           </S.Title>
           <S.Description>famílias</S.Description>
         </S.Content>
         <S.Content>
           <GiHeavyArrow size={40} />
           <S.Title>
-            <CountUp start={0} end={70} duration={2} prefix="+" />
+            <CountUp redraw end={70} duration={1.3} prefix="+">
+              {({ countUpRef, start }) => (
+                <VisibilitySensor onChange={start} delayedCall>
+                  <span ref={countUpRef} />
+                </VisibilitySensor>
+              )}
+            </CountUp>
           </S.Title>
           <S.Description>missionários</S.Description>
         </S.Content>
         <S.Content>
           <MdVolunteerActivism size={40} />
           <S.Title>
-            <CountUp start={0} end={100} duration={2} prefix="+" />
+            <CountUp end={100} duration={1.3} prefix="+">
+              {({ countUpRef, start }) => (
+                <VisibilitySensor onChange={start} delayedCall>
+                  <span ref={countUpRef} />
+                </VisibilitySensor>
+              )}
+            </CountUp>
           </S.Title>
           <S.Description>voluntários</S.Description>
         </S.Content>
