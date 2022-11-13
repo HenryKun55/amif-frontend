605e34d893f9393d2bf76ed15a37c245585da0a6
Flávio Andrade <flavio.andrade@distrito.me>
2022-11-13 11:14:54 -0300

    feat: add scroll to top when change route

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,9 @@
 import { ModalProvider } from 'context/Modal/index.provider'
 import { ptBR } from 'date-fns/locale'
 import SetDefaultOptions from 'date-fns/setDefaultOptions'
+import { useEffect } from 'react'
 import { Provider } from 'react-redux'
+import { useLocation } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 
 import { Router } from '@/routes'
@@ -9,6 +11,12 @@ import store from '@/store'
 import { GlobalStyles } from '@/styles/global'
 
 export const App = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   SetDefaultOptions({ locale: ptBR })
 
   return (
