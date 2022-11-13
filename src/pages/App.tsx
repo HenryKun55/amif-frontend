58de29093f6aa6f175d42a63d0e67ffaf3617ce1
Flávio Andrade <flavio.andrade@distrito.me>
2022-11-13 11:21:12 -0300

    feat: add unused-imports

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,7 @@
 import { ModalProvider } from 'context/Modal/index.provider'
 import { ptBR } from 'date-fns/locale'
 import SetDefaultOptions from 'date-fns/setDefaultOptions'
-import { useEffect } from 'react'
 import { Provider } from 'react-redux'
-import { useLocation } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 
 import { Router } from '@/routes'
@@ -11,12 +9,6 @@ import store from '@/store'
 import { GlobalStyles } from '@/styles/global'
 
 export const App = () => {
-  const { pathname } = useLocation()
-
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [pathname])
-
   SetDefaultOptions({ locale: ptBR })
 
   return (
