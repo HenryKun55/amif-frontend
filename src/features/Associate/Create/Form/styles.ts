2b45239411f7a6a30db9da7581a0c70dbbe4517c
Ricardo Mendes <sr_ricardomendes@hotmail.com>
2022-11-22 21:48:45 -0300

    form componentization

--- src/features/Associate/Create/Form/styles.ts
+++ src/features/Associate/Create/Form/styles.ts
@@ -1,13 +1,12 @@
 import tw, { styled } from 'twin.macro'
 
 import { Button as ButtonComponent } from '@/components/Form/Button'
-import { Input as InputComponent, InputAs } from '@/components/Form/Input'
-
-import type { FormProps } from './validator'
+import { Input as InputComponent } from '@/components/Form/Input'
 
 export const Form = styled.form([
-  tw`flex flex-col w-full h-full max-w-content gap-4`,
+  tw`flex flex-col w-full h-full max-w-content gap-8`,
 ])
+export const Wrapper = styled.div([tw`flex flex-col w-full gap-4`])
 
 export const Row = styled.div([
   tw`w-full flex flex-col md:flex-row items-center gap-4`,
@@ -17,7 +16,7 @@ export const Title = styled.h3(
   tw`font-bold text-3xl text-brown-500 underline my-2.5`,
 )
 
-export const Input = styled(InputComponent)(tw`max-w-2xl`) as InputAs<FormProps>
+export const Input = styled(InputComponent)(tw`max-w-2xl`)
 
 export const Button = styled(ButtonComponent)(tw`w-full
   md:w-4/12
